467c387e4a3c35f98df4e539b32f2a08f3886719
suyong choi <dpfprtus@gmail.com>
2024-01-23 14:56:45 +0900

    [FEAT] Search 에러

--- open_weather_provider/lib/pages/home_page.dart
+++ open_weather_provider/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:open_weather_provider/pages/search_page.dart';
+import 'package:open_weather_provider/widgets/error_dialog.dart';
 import 'package:provider/provider.dart';
 
 import 'package:open_weather_provider/providers/weather/weather_provider.dart';
@@ -12,6 +13,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final WeatherProvider _weatherProvider;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _weatherProvider = context.read<WeatherProvider>();
+    _weatherProvider.addListener(_registerListener);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _weatherProvider.removeListener(_registerListener);
+    super.dispose();
+  }
+
+  void _registerListener() {
+    final WeatherState ws = context.read<WeatherProvider>().state;
+    if (ws.status == WeatherStatus.error) {
+      errorDialog(context, ws.error.errMsg);
+    }
+  }
+
   Widget _showWeather() {
     final state = context.watch<WeatherProvider>().state;
     if (state.status == WeatherStatus.initial) {
@@ -28,6 +52,23 @@ class _HomePageState extends State<HomePage> {
         child: CircularProgressIndicator(),
       );
     }
+    if (state.status == WeatherStatus.error && state.weather.name == '') {
+      return Center(
+        child: Text(
+          'Select a city',
+          style: TextStyle(fontSize: 20),
+        ),
+      );
+    }
+
+    return Center(
+      child: Text(
+        state.weather.name,
+        style: TextStyle(
+          fontSize: 18,
+        ),
+      ),
+    );
   }
 
   String? _city;
