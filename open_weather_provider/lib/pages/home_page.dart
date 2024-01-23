4ef4288303ef2b9f05590de5a261d42c798a191b
suyong choi <dpfprtus@gmail.com>
2024-01-23 16:00:36 +0900

    [FEAT] 메인페이지

--- open_weather_provider/lib/pages/home_page.dart
+++ open_weather_provider/lib/pages/home_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:open_weather_provider/constants/constants.dart';
 import 'package:open_weather_provider/pages/search_page.dart';
 import 'package:open_weather_provider/widgets/error_dialog.dart';
 import 'package:provider/provider.dart';
 
 import 'package:open_weather_provider/providers/weather/weather_provider.dart';
+import 'package:recase/recase.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -36,6 +38,10 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  String showTemparature(double temparature) {
+    return temparature.toStringAsFixed(2) + '℃';
+  }
+
   Widget _showWeather() {
     final state = context.watch<WeatherProvider>().state;
     if (state.status == WeatherStatus.initial) {
@@ -61,13 +67,103 @@ class _HomePageState extends State<HomePage> {
       );
     }
 
-    return Center(
-      child: Text(
-        state.weather.name,
-        style: TextStyle(
-          fontSize: 18,
+    return ListView(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 6,
         ),
-      ),
+        Text(
+          state.weather.name,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              TimeOfDay.fromDateTime(state.weather.lastUpdated).format(context),
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              '(${state.weather.country})',
+              style: TextStyle(fontSize: 18),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 60,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              showTemparature(state.weather.temp),
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Column(
+              children: [
+                Text(
+                  showTemparature(state.weather.tempMax),
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  showTemparature(state.weather.tempMin),
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            )
+          ],
+        ),
+        SizedBox(
+          height: 40,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Spacer(),
+            showIcon(state.weather.icon),
+            Expanded(flex: 3, child: formatText(state.weather.description)),
+            Spacer(),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget showIcon(String icon) {
+    //이미지를 받아오기 까지 로딩되는 이미지
+    return FadeInImage.assetNetwork(
+      placeholder: 'assets/images/loading.gif',
+      image: 'http://$kIconHost/img/wn/$icon@4x.png',
+      width: 96,
+      height: 96,
+    );
+  }
+
+  Widget formatText(String description) {
+    //recase 패키지를 이용해 매 단어의 시작이 대문자인 titleCase로 변환.
+    final formattedString = description.titleCase;
+    return Text(
+      formattedString,
+      style: TextStyle(fontSize: 24),
+      textAlign: TextAlign.center,
     );
   }
 
